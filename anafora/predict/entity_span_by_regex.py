8b0b65646e50005c7a12f040c5b3c7041d0371dd
Steven Bethard <steven.bethard@gmail.com>
2014-09-03 17:15:09 -0500

    Adds first draft of a regex dictionary annotator for entities

---
+++ anafora/predict/entity_span_by_regex.py
@@ -0,0 +1,40 @@
+import argparse
+import os
+import re
+
+import anafora
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--encoding", default="utf-8")
+    parser.add_argument("entity_type")
+    parser.add_argument("regex_file")
+    parser.add_argument("input_dir")
+    parser.add_argument("output_dir")
+    args = parser.parse_args()
+
+    with open(args.regex_file) as regex_file:
+        lines = [line.rstrip("\r\n") for line in regex_file.readlines()]
+        regex = re.compile(r"\b" + r"\b|\b".join(lines) + r"\b", re.IGNORECASE)
+
+    for dir_name in os.listdir(args.input_dir):
+        file_name = dir_name
+        text_path = os.path.join(args.input_dir, dir_name, file_name)
+        with open(text_path) as text_file:
+            text = text_file.read().decode(args.encoding)
+
+        data = anafora.AnaforaData()
+        for i, match in enumerate(regex.finditer(text.lower())):
+            start, end = match.span()
+            annotation = anafora.AnaforaEntity()
+            annotation.id = "{0}@{1}".format(i, file_name)
+            annotation.type = args.entity_type
+            annotation.spans = ((start, end),)
+            data.annotations.append(annotation)
+
+        output_dir = os.path.join(args.output_dir, file_name)
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        output_path = os.path.join(output_dir, file_name + ".xml")
+        with open(output_path, 'w') as output_file:
+            anafora.ElementTree.ElementTree(data.xml).write(output_file)
