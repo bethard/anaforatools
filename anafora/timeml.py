e53b69e8a7245467c334c95eb926052031111ef8
Steven Bethard <steven.bethard@gmail.com>
2014-09-03 10:53:25 -0500

    Makes anafora.timeml executable for converting TimeML annotations to Anafora annotations

--- anafora/timeml.py
+++ anafora/timeml.py
@@ -1,3 +1,6 @@
+import argparse
+import os
+
 import anafora
 
 
@@ -60,9 +63,46 @@ def to_anafora_data(timeml_path):
             offset += len(elem.tail)
     return data
 
-#if __name__ == "__main__":
-#    for timeml_dir in sys.argv[1:]:
-#        for timeml_file in os.listdir(timeml_dir):
-#            timeml_path = os.path.join(timeml_dir, timeml_file)
-#            timeml_elem = anafora.ElementTree.parse(timeml_path).getroot()
-#            data = to_anafora_data(timeml_elem)
+# http://effbot.org/zone/element-lib.htm#prettyprint
+def _indent(elem, level=0):
+    i = "\n" + level * "  "
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + "  "
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+        for elem in elem:
+            _indent(elem, level + 1)
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = i
+
+def _timeml_dir_to_anafora_dir(timeml_dir, anafora_dir):
+    for root, _, file_names in os.walk(timeml_dir):
+        for file_name in file_names:
+            if file_name.endswith(".tml"):
+                file_path = os.path.join(root, file_name)
+                text = to_text(file_path)
+                data = to_anafora_data(file_path)
+                _indent(data.xml)
+
+                anafora_file_name = file_name[:-4]
+                anafora_file_dir = os.path.join(anafora_dir, anafora_file_name)
+                if not os.path.exists(anafora_file_dir):
+                    os.makedirs(anafora_file_dir)
+                anafora_file_path = os.path.join(anafora_file_dir, anafora_file_name)
+
+                with open(anafora_file_path, 'w') as text_file:
+                    text_file.write(text)
+                with open(anafora_file_path + ".timeml.timeml.gold.completed.xml", 'w') as xml_file:
+                    anafora.ElementTree.ElementTree(data.xml).write(xml_file)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("timeml_dir")
+    parser.add_argument("anafora_dir")
+    args = parser.parse_args()
+    _timeml_dir_to_anafora_dir(args.timeml_dir, args.anafora_dir)
