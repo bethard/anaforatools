6cfb54b376394d2e459b5b1986d7c8bc0859d230
Steven Bethard <steven.bethard@gmail.com>
2014-09-09 21:29:08 -0500

    Replaces use of ElementTree.iterparse with .parse since .iterparse was producing an empty .tail in some situations (possibly large files?) on the TimeBank

--- anafora/timeml.py
+++ anafora/timeml.py
@@ -17,12 +17,11 @@ def to_anafora_data(timeml_path):
     :param xml.etree.ElementTree.Element timeml_path: path of the TimeML XML
     :return anafora.AnaforaData: an Anafora version of the TimeML annotations
     """
-    entity_id_attrs = {
+    entity_tags = {"TIMEX3", "EVENT", "SIGNAL"}
+    tag_id_attrs = {
         "TIMEX3": "tid",
         "EVENT": "eid",
         "SIGNAL": "sid",
-    }
-    relation_id_attrs = {
         "MAKEINSTANCE": "eiid",
         "TLINK": "lid",
         "SLINK": "lid",
@@ -30,40 +29,37 @@ def to_anafora_data(timeml_path):
     }
     text = to_text(timeml_path)
     data = anafora.AnaforaData()
-    offset = 0
-    for event, elem in anafora.ElementTree.iterparse(timeml_path, events=("start", "end")):
-        if event == "start":
-            annotation = None
-            if elem.tag in entity_id_attrs:
-                id_attrs = entity_id_attrs
-                annotation = anafora.AnaforaEntity()
-            elif elem.tag in relation_id_attrs:
-                id_attrs = relation_id_attrs
-                annotation = anafora.AnaforaRelation()
 
-            if annotation is not None:
-                id_attr = id_attrs[elem.tag]
-                annotation.id = elem.attrib[id_attr]
-                annotation.type = elem.tag
-                if elem.tag in entity_id_attrs:
-                    annotation.spans = ((offset, offset),)
-                for name, value in elem.attrib.items():
-                    if name != id_attr:
-                        annotation.properties[name] = value
-                data.annotations.append(annotation)
+    def add_annotations_from(elem, offset=0):
+        start = offset
+        annotation = None
+        if elem.tag in tag_id_attrs:
+            annotation = anafora.AnaforaEntity() if elem.tag in entity_tags else anafora.AnaforaRelation()
+            id_attr = tag_id_attrs[elem.tag]
+            annotation.id = elem.attrib[id_attr]
+            annotation.type = elem.tag
+            if isinstance(annotation, anafora.AnaforaEntity):
+                annotation.spans = ((start, start),)
+            for name, value in elem.attrib.items():
+                if name != id_attr:
+                    annotation.properties[name] = value
+            data.annotations.append(annotation)
 
-        elif event == "end" and elem.tag in entity_id_attrs:
-            annotation_id = elem.attrib[entity_id_attrs[elem.tag]]
-            annotation = data.annotations.select_id(annotation_id)
-            (start, _), = annotation.spans
+        if elem.text is not None:
+            offset += len(elem.text)
+        for child in elem:
+            offset = add_annotations_from(child, offset)
+
+        if annotation is not None and isinstance(annotation, anafora.AnaforaEntity):
             annotation.spans = ((start, offset),)
             if elem.text != text[start:offset]:
-                raise ValueError('{0}: "{1}" != "{2} ({3})"'.format(timeml_path, elem.text, text[start:offset], elem.attrib))
+                raise ValueError('{0}: "{1}" != "{2}"'.format(timeml_path, elem.text, text[start:offset]))
 
-        if event == "start" and elem.text is not None:
-            offset += len(elem.text)
-        if event == "end" and elem.tail is not None:
+        if elem.tail is not None:
             offset += len(elem.tail)
+        return offset
+
+    add_annotations_from(anafora.ElementTree.parse(timeml_path).getroot())
     return data
 
 
