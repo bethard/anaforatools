c6a260ca6f181cd6fad6a1a4fd18364f759aec0a
Steven Bethard <steven.bethard@gmail.com>
2014-10-19 10:33:14 -0400

    Improves documentation and command line interface of anafora.cleanup

--- anafora/cleanup.py
+++ anafora/cleanup.py
@@ -5,32 +5,39 @@ import os
 import anafora
 import anafora.validate
 
-def fix_thyme_errors(schema, input_dir, xml_name_regex, output_dir):
+def fix_thyme_errors(schema, input_dir, output_dir, xml_name_regex="[.]xml$"):
     """
-    :param schema anafora.validate.Schema:
-    :param input_dir str:
-    :param output_dir str:
-    :return:
+    :param schema anafora.validate.Schema: the THYME schema
+    :param input_dir str: the root of a set of THYME Anafora XML directories
+    :param output_dir str: the directory where the cleaned versions of the THYME Anafora XML files should be written.
+        The directory structure will mirror the input directory structure.
     """
     for sub_dir, text_name, xml_names in anafora.walk(input_dir, xml_name_regex):
         for xml_name in xml_names:
             xml_path = os.path.join(input_dir, sub_dir, xml_name)
-            data = anafora.AnaforaData.from_file(xml_path)
+
+            # load the data from the Anafora XML
+            try:
+                data = anafora.AnaforaData.from_file(xml_path)
+            except anafora.ElementTree.ParseError as e:
+                logging.warning("SKIPPING invalid XML: %s: %s", e.message, xml_path)
+                continue
+
+            # remove invalid TLINKs and ALINKs
             changed = False
-            while True:
-                to_remove = []
-                for annotation in data.annotations:
-                    try:
-                        schema.validate(annotation)
-                    except anafora.validate.SchemaValidationError as e:
-                        if annotation.type in {"TLINK", "ALINK"}:
-                            logging.warning("%s: REMOVING %s", e.message, annotation)
-                            to_remove.append(annotation)
-                if not to_remove:
-                    break
-                for annotation in to_remove:
-                    data.annotations.remove(annotation)
-                    changed = True
+            to_remove = []
+            for annotation in data.annotations:
+                try:
+                    schema.validate(annotation)
+                except anafora.validate.SchemaValidationError as e:
+                    if annotation.type in {"TLINK", "ALINK"}:
+                        logging.warning("REMOVING %s: %s", e.message, annotation)
+                        to_remove.append(annotation)
+            for annotation in to_remove:
+                data.annotations.remove(annotation)
+                changed = True
+
+            # remove TIMEX3s that are directly on top of SECTIONTIMEs and DOCTIMEs
             for span, annotations in anafora.validate.find_entities_with_identical_spans(data):
                 try:
                     # sorts SECTIONTIME and DOCTIME before TIMEX3
@@ -39,7 +46,7 @@ def fix_thyme_errors(schema, input_dir, xml_name_regex, output_dir):
                     pass
                 else:
                     if special_time.type in {"SECTIONTIME", "DOCTIME"} and timex.type == "TIMEX3":
-                        msg = "multiple entities for span %s: REPLACING %s WITH %s"
+                        msg = "REPLACING multiple entities for span %s: %s WITH %s"
                         logging.warning(msg, span, timex, special_time)
                         for annotation in data.annotations:
                             for name, value in annotation.properties.items():
@@ -47,6 +54,8 @@ def fix_thyme_errors(schema, input_dir, xml_name_regex, output_dir):
                                     annotation.properties[name] = special_time
                         data.annotations.remove(timex)
                         changed = True
+
+            # if we found and fixed any errors, write out the new XML file
             if changed:
                 output_sub_dir = os.path.join(output_dir, sub_dir)
                 if not os.path.exists(output_sub_dir):
@@ -55,21 +64,25 @@ def fix_thyme_errors(schema, input_dir, xml_name_regex, output_dir):
                 data.to_file(output_path)
 
 
-
 if __name__ == "__main__":
-    choices = {
-        "thyme": fix_thyme_errors,
-    }
-
     parser = argparse.ArgumentParser()
-    parser.add_argument("--type", choices=choices, required=True)
-    parser.add_argument("--schema", required=True)
-    parser.add_argument("--input-dir", required=True)
-    parser.add_argument("--xml-name-regex", default="[.]xml$")
-    parser.add_argument("--output-dir", required=True)
-    args = parser.parse_args()
+    subparsers = parser.add_subparsers()
 
+    thyme_parser = subparsers.add_parser('thyme')
+    thyme_parser.set_defaults(func=fix_thyme_errors)
+    thyme_parser.add_argument("-s", "--schema", metavar="FILE", required=True, type=anafora.validate.Schema.from_file,
+                              help="An Anafora schema XML file which Anafora annotation XML files will be validated " +
+                                   "against.")
+    thyme_parser.add_argument("-i", "--input", metavar="DIR", required=True, dest="input_dir",
+                              help="The root of a set of Anafora annotation XML directories.")
+    thyme_parser.add_argument("-o", "--output", metavar="DIR", required=True, dest="output_dir",
+                              help="The directory where the cleaned versions of the Anafora annotation XML files " +
+                                   "should be written. The directory structure will mirror the input directory " +
+                                   "structure.")
+    thyme_parser.add_argument("-x", "--xml-name-regex", metavar="REGEX", default="[.]xml$",
+                              help="A regular expression for matching XML files, typically used to restrict the " +
+                                   "validation to a subset of the available files (default: %(default)r)")
+
+    args = parser.parse_args()
     kwargs = vars(args)
-    func = choices[kwargs.pop('type')]
-    schema = anafora.validate.Schema.from_file(kwargs.pop("schema"))
-    func(schema, **kwargs)
+    kwargs.pop('func')(**kwargs)
