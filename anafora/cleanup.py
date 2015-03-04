300b7d2f38c2c9bd2f6be8aab594ac8246d335f5
Steven Bethard <steven.bethard@gmail.com>
2015-03-04 12:51:38 -0600

    Starts work on parsing from the THYME question-answer format to the Anafora XML format

--- anafora/cleanup.py
+++ anafora/cleanup.py
@@ -1,6 +1,9 @@
+from __future__ import absolute_import
+
 import argparse
 import logging
 import os
+import regex
 
 import anafora
 import anafora.validate
@@ -64,6 +67,57 @@ def fix_thyme_errors(schema, input_dir, output_dir, xml_name_regex="[.]xml$"):
                 data.to_file(output_path)
 
 
+def convert_thyme_qa_to_anafora_xml(input_dir, output_dir):
+    _header_sep_pattern = regex.compile(r'\s*=====+\s*')
+    _annotation_sep_pattern = regex.compile(r'\s*-----+\s*')
+    _annotation_pattern = regex.compile(r'^Question:(.*?)\nAnswer:(.*?)\nConfidence:(.*?)\n' +
+                                        r'Difficulty:(.*?)\nDocTimeRel:(.*?)\n(Text Clip:.*)$', regex.DOTALL)
+    _text_clip_pattern = regex.compile(r'Text Clip:\s+\d+[.]\s+(\d+),(\d+) (Exact|Support)_Answer ' +
+                                       r'Use_(Time_Span|DocTimeRel) (.*)\n(.*)(?:\n|$)')
+
+    # iterate through all _qa.txt files in the input directory
+    for input_root, dir_names, input_file_names in os.walk(input_dir):
+        for input_file_name in input_file_names:
+            if input_file_name.endswith("_qa.txt"):
+                file_base = input_file_name[:-7]
+
+                # create one Anafora XML for each file
+                data = anafora.AnaforaData()
+                count = 1
+                with open(os.path.join(input_root, input_file_name)) as input_file:
+                    text = input_file.read().decode('ascii')
+
+                    # parse the annotations from the THYME question-answer format
+                    _, body_text = _header_sep_pattern.split(text)
+                    for annotation_text in _annotation_sep_pattern.split(body_text.rstrip(" \n\r-")):
+                        match = _annotation_pattern.match(annotation_text)
+                        if match is None:
+                            raise ValueError("Invalid annotation text: " + annotation_text)
+                        question, answer, confidence, difficulty, doc_time_rel, text_clip_text = match.groups()
+                        text_clip_matches = _text_clip_pattern.findall(text_clip_text)
+                        if len(text_clip_text.splitlines()) != 2 * len(text_clip_matches):
+                            raise ValueError("Invalid Text Clips in annotation text: " + annotation_text)
+
+                        # create Anafora XML annotations from the THYME question-answer annotations
+                        for begin_text, end_text, _, time_or_doc_time_rel, type_text, clip_text in text_clip_matches:
+                            begin = int(begin_text)
+                            end = int(end_text)
+                            entity = anafora.AnaforaEntity()
+                            entity.id = '{0:d}@{1}@{2}@gold'.format(count, 'e', file_base)
+                            entity.spans = ((begin, end),)
+                            # TODO: decode type_text and generate corresponding attributes/annotations
+                            data.annotations.append(entity)
+                            count += 1
+
+                # write the Anafora data out as XML
+                output_file_dir = os.path.join(output_dir, file_base)
+                output_file_path = os.path.join(output_file_dir, file_base + ".xml")
+                if not os.path.exists(output_file_dir):
+                    os.makedirs(output_file_dir)
+                data.indent()
+                data.to_file(output_file_path)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers()
@@ -82,6 +136,13 @@ if __name__ == "__main__":
     thyme_parser.add_argument("-x", "--xml-name-regex", metavar="REGEX", default="[.]xml$",
                               help="A regular expression for matching XML files, typically used to restrict the " +
                                    "validation to a subset of the available files (default: %(default)r)")
+
+    thyme_qa_parser = subparsers.add_parser('thyme-qa')
+    thyme_qa_parser.set_defaults(func=convert_thyme_qa_to_anafora_xml)
+    thyme_qa_parser.add_argument("-i", "--input", metavar="DIR", required=True, dest="input_dir",
+                                 help="The root of a set of THYME _qa.txt files.")
+    thyme_qa_parser.add_argument("-o", "--output", metavar="DIR", required=True, dest="output_dir",
+                                 help="The directory where the Anafora annotation XML files should be written.")
 
     args = parser.parse_args()
     kwargs = vars(args)
