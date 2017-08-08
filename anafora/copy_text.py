3c5cab4139ad219a1032fd895fe6b66e0c84696a
Steven Bethard <bethard@email.arizona.edu>
2017-08-08 09:21:12 -0700

    Adds support for copying plain text files into Anafora directory structure

--- anafora/copy_text.py
+++ anafora/copy_text.py
@@ -7,39 +7,60 @@ import anafora
 import anafora.timeml
 
 
-def copy_timeml_text(timeml_dir, anafora_dir, xml_name_regex):
-    timeml_paths = {}
-    for dir_path, _, file_names in os.walk(timeml_dir):
+def copy_timeml_text(text_dir, anafora_dir, xml_name_regex):
+    text_name_to_path = {}
+    for dir_path, _, file_names in os.walk(text_dir):
         for file_name in file_names:
             if file_name.endswith(".tml"):
-                timeml_paths[file_name[:-4]] = os.path.join(dir_path, file_name)
+                file_path = os.path.join(dir_path, file_name)
+                text_name_to_path[file_name[:-4]] = file_path
+    _copy_text(text_name_to_path=text_name_to_path,
+               get_text=anafora.timeml.to_text,
+               anafora_dir=anafora_dir,
+               xml_name_regex=xml_name_regex)
 
+
+def copy_plain_text(text_dir, anafora_dir, xml_name_regex):
+    text_name_to_path = {}
+    for dir_path, _, file_names in os.walk(text_dir):
+        for file_name in file_names:
+            text_name_to_path[file_name] = os.path.join(dir_path, file_name)
+    _copy_text(text_name_to_path=text_name_to_path,
+               get_text=lambda path: open(path).read(),
+               anafora_dir=anafora_dir,
+               xml_name_regex=xml_name_regex)
+
+
+def _copy_text(text_name_to_path, get_text, anafora_dir, xml_name_regex):
     for sub_dir, text_file_name, _ in anafora.walk(
             anafora_dir, xml_name_regex=xml_name_regex):
-        if text_file_name not in timeml_paths:
-            sys.exit("No .tml file found for " + text_file_name)
-        text = anafora.timeml.to_text(timeml_paths[text_file_name])
+        if text_file_name not in text_name_to_path:
+            sys.exit("No text file found for " + text_file_name)
         text_path = os.path.join(anafora_dir, sub_dir, text_file_name)
         if os.path.exists(text_path):
             sys.exit("Text file already exists: " + text_path)
+        text = get_text(text_name_to_path[text_file_name])
         with open(text_path, 'w') as text_file:
             text_file.write(text)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers()
+    format_funcs = dict(plain=copy_plain_text, timeml=copy_timeml_text)
 
-    timeml_parser = subparsers.add_parser("timeml")
-    timeml_parser.set_defaults(func=copy_timeml_text)
-    timeml_parser.add_argument(
+    parser = argparse.ArgumentParser(
+        description="Copies text files to their expected locations in an " +
+                    "Anafora XML directory hierarchy. That is, each text " +
+                    "file that exactly matches the name of a directory in " +
+                    "the Anafora XML directory hierarchy will be copied into " +
+                    "that directory.")
+    parser.add_argument(
         "-x", "--xml-name-regex",
         metavar="REGEX", default=".*completed.*[.]xml$",
-        help="A regular expression for matching XML files in the Anafora " +
-             "subdirectories (default: %(default)r)")
-    timeml_parser.add_argument("timeml_dir")
-    timeml_parser.add_argument("anafora_dir")
+        help="The regular expression which identifies Anafora XML files " +
+             "(default: %(default)r).")
+    parser.add_argument("--format", choices=format_funcs, default="plain")
+    parser.add_argument("text_dir")
+    parser.add_argument("anafora_dir")
 
-    args = parser.parse_args()
-    kwargs = vars(args)
-    kwargs.pop("func")(**kwargs)
+    kwargs = vars(parser.parse_args())
+    format_funcs[kwargs.pop("format")](**kwargs)
